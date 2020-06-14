da97921290beac7649f2064f06865ad8376978ff
jiwon <jiwonov@gmail.com>
2020-06-14 19:02:14 +0900

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,28 @@ $(document).ready(function() {
     e.preventDefault();
     $('.js-off-canvas-toggle').toggleClass('is-active');
     $('.js-off-canvas-container').toggleClass('is-active');
+
+    // IE Check
+    if (window.document.documentMode) {
+      let navigation = $('.c-off-canvas-content');
+      if (!navigation.attr('data-ie')) {
+        navigation.data('ie', window.document.documentMode);
+
+        navigation.css({
+          'transition':'transform .4s cubic-bezier(0.16, 0.68, 0.43, 0.99)',
+          'transform':'translate3d(300px, 0px, 0px)',
+          'z-index':'1'
+        });
+      } else {
+        navigation.removeAttr('data-ie')
+
+        navigation.css({
+          'transition':'transform .4s cubic-bezier(0.16, 0.68, 0.43, 0.99)',
+          'transform':'translate3d(0px, 0px, 0px)',
+          'z-index':'1'
+        });
+      }
+    }
   });
 
 });
