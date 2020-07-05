849ecc9848f2fd3f691d84d14030442cc1830a9d
jiwon <jiwonov@gmail.com>
2020-07-05 16:46:52 +0900

    Update IE navigation event

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,28 @@ $(document).ready(function() {
   // Off Canvas menu
   // =================
 
+  // IE Canvas event
+  function ieCanvas(navigation) {
+    if (!navigation.attr('data-ie')) {
+      navigation.attr('data-ie', window.document.documentMode);
+
+      navigation.css({
+        'transition':'transform .4s cubic-bezier(0.16, 0.68, 0.43, 0.99)',
+        'transform':'translate3d(0px, 0px, 0px)',
+        'z-index':'1'
+      });
+    } else {
+      navigation.removeAttr('data-ie')
+
+      navigation.css({
+        'transition':'transform .4s cubic-bezier(0.16, 0.68, 0.43, 0.99)',
+        'transform':'translate3d(300px, 0px, 0px)',
+        'z-index':'1'
+      });
+    }
+  }
+
+  // Toggle button event
   $('.js-off-canvas-toggle').click(function(e) {
     e.preventDefault();
     $('.js-off-canvas-toggle').toggleClass('is-active');
@@ -24,33 +46,23 @@ $(document).ready(function() {
 
     // IE Check
     if (window.document.documentMode) {
-      let navigation = $('.c-off-canvas-content');
-
-      if (!navigation.attr('data-ie')) {
-        navigation.attr('data-ie', window.document.documentMode);
-
-        navigation.css({
-          'transition':'transform .4s cubic-bezier(0.16, 0.68, 0.43, 0.99)',
-          'transform':'translate3d(0px, 0px, 0px)',
-          'z-index':'1'
-        });
-      } else {
-        navigation.removeAttr('data-ie')
-
-        navigation.css({
-          'transition':'transform .4s cubic-bezier(0.16, 0.68, 0.43, 0.99)',
-          'transform':'translate3d(300px, 0px, 0px)',
-          'z-index':'1'
-        });
-      }
+      ieCanvas($('.c-off-canvas-content'));
     }
   });
 
+  // Canvas left blank event
   $('.c-off-canvas-blank').click(function(e) {
     if ($(this).hasClass('is-active')) {
       $('.js-off-canvas-toggle').toggleClass('is-active');
       $('.js-off-canvas-container').toggleClass('is-active');
+
+      // Canvas menu left blank
       $(this).toggleClass('is-active');
+
+      // IE Check
+      if (window.document.documentMode) {
+        ieCanvas($('.c-off-canvas-content'));
+      }
     }
   });
 
