ded3980e87dd89305ce00618694105a8240969f3
jiwon <jiwonov@gmail.com>
2020-11-10 10:31:51 +0900

    Update ajax logic

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.body.style.pointerEvents = 'auto';
 /* IE Check */
 if (document["documentMode"]) {
     document.body.innerHTML =
-        "<div id='noexplorer'>"
+        "<div id='is-explorer'>"
             + "<span>DESSIN.DEV no longer supports this web browser. "
                 + "<a href='microsoft-edge:https://dessin.dev'>Click here to launch the site in the MS Edge browser</a>"
             + "</span>"
@@ -15,8 +15,13 @@ if (document["documentMode"]) {
 /* json data for search */
 let articles = [];
 (function() {
-    // IE Check
+    // IE check
     if (document["documentMode"]) {
+        return;
+    }
+
+    // Index page check
+    if (location.pathname !== '/' && location.hostname !== 'localhost') {
         return;
     }
 
@@ -118,63 +123,60 @@ function searchArticles(_this) {
 
 /* Get Articles By AJAX */
 function getArticles() {
-    // IE Check
-    if (document["documentMode"]) {
-        return;
-    }
-
-    // Show ration animation
-    if (document.getElementById('rotate-wrap').style.display === 'none' && parseInt(document.getElementById('rotate-wrap').style.opacity) === 0) {
-        document.getElementById('rotate-wrap').style.display = 'block';
-        setTimeout(function() {
-            document.getElementById('rotate-wrap').style.opacity = '1';
-        }, 100);
-    }
-
-    // Hide ration animation & Articles data processing
-    setTimeout(function() {
-        // Hide rotation animation
-        document.getElementById('rotate-wrap').style.opacity = '0';
-        setTimeout(function() {
-            document.getElementById('rotate-wrap').style.display = 'none';
-        }, 200);
-
-        // Get page number
-        let page = parseInt(document.getElementById('pagination').getAttribute('data-page'));
-
-        // for start idx
-        let start_idx = (page === 1) ? 0 : document.getElementById('article').getElementsByTagName('li').length;
-
-        // for end idx
-        let end_idx = (page * 5 > articles.length) ? articles.length : page * 5;
-
-        // Limit 10 page
-        if (page >= 10) {
-            alert('더 이상 불러올 수 없습니다.');
-            return;
+    if (document.getElementById('rotate-wrap')) {
+        // Show ration animation
+        if (document.getElementById('rotate-wrap').style.display === 'none' && parseInt(document.getElementById('rotate-wrap').style.opacity) === 0) {
+            document.getElementById('rotate-wrap').style.display = 'block';
+            setTimeout(function() {
+                document.getElementById('rotate-wrap').style.opacity = '1';
+            }, 100);
         }
 
-        // Get all articles
-        if (start_idx >= end_idx) {
-            alert('모든 포스트를 불러왔습니다.');
-            return;
-        }
+        // Hide ration animation & Articles data processing
+        setTimeout(function() {
+            // Hide rotation animation
+            document.getElementById('rotate-wrap').style.opacity = '0';
+            setTimeout(function() {
+                document.getElementById('rotate-wrap').style.display = 'none';
+            }, 200);
 
-        // document.getElementById('article').getElementsByTagName('ul')[0].innerHTML = '';
-        for (let idx = start_idx; idx < end_idx; idx++) {
-            if (idx < 5) {
-                // Initialization DOM matching
-                document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('title')[0].setAttribute('href', articles[idx]['link']);
-                document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('title')[0].innerHTML = articles[idx]['title'];
-                document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('content')[0].innerHTML = articles[idx]['description'];
-                document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('datetime')[0].innerHTML = articles[idx]['pubDate'];
-            } else {
-                // 5+ DOM Matching
-                createArticleDOM(idx);
+            // Get page number
+            let page = parseInt(document.getElementById('pagination').getAttribute('data-page'));
+
+            // for start idx
+            let start_idx = (page === 1) ? 0 : document.getElementById('article').getElementsByTagName('li').length;
+
+            // for end idx
+            let end_idx = (page * 5 > articles.length) ? articles.length : page * 5;
+
+            // Limit 10 page
+            if (page >= 10) {
+                alert('더 이상 불러올 수 없습니다.');
+                return;
             }
-        }
 
-        // Add 1 page
-        document.getElementById('pagination').setAttribute('data-page', (page + 1).toString());
-    }, 700);
+            // Get all articles
+            if (start_idx >= end_idx) {
+                alert('모든 포스트를 불러왔습니다.');
+                return;
+            }
+
+            // document.getElementById('article').getElementsByTagName('ul')[0].innerHTML = '';
+            for (let idx = start_idx; idx < end_idx; idx++) {
+                if (idx < 5) {
+                    // Initialization DOM matching
+                    document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('title')[0].setAttribute('href', articles[idx]['link']);
+                    document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('title')[0].innerHTML = articles[idx]['title'];
+                    document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('content')[0].innerHTML = articles[idx]['description'];
+                    document.getElementById('article').getElementsByTagName('li')[idx].getElementsByClassName('datetime')[0].innerHTML = articles[idx]['pubDate'];
+                } else {
+                    // 5+ DOM Matching
+                    createArticleDOM(idx);
+                }
+            }
+
+            // Add 1 page
+            document.getElementById('pagination').setAttribute('data-page', (page + 1).toString());
+        }, 700);
+    }
 }
